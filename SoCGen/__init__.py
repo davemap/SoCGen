47891dfa613c3deba694e83fd99ec4654c82cb65
David Mapstone <david@mapstone.me>
2022-11-07 14:46:13 +0000

    fix: Updated testpath ini init.py

--- SoCGen/__init__.py
+++ SoCGen/__init__.py
@@ -2,5 +2,5 @@ from SoCGen.interface import *
 from SoCGen.connection import *
 from SoCGen.module import *
 from SoCGen.parameter import *
-from test import *
+from Test.test import *
 from SoCGen.sv_gen import *
